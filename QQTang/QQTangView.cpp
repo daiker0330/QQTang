cf8819462547500992e61217b00b32aac7b971d5
DarkForte <laserae86@126.com>
2015-03-18 11:46:22 +0800

    新建工程
    新建了MFC工程，无最大化，单文档，细框架
    准备去除没用的xx栏

---
+++ QQTang/QQTangView.cpp
@@ -0,0 +1,96 @@
+
+// QQTangView.cpp : CQQTangView 类的实现
+//
+
+#include "stdafx.h"
+// SHARED_HANDLERS 可以在实现预览、缩略图和搜索筛选器句柄的
+// ATL 项目中进行定义，并允许与该项目共享文档代码。
+#ifndef SHARED_HANDLERS
+#include "QQTang.h"
+#endif
+
+#include "QQTangDoc.h"
+#include "QQTangView.h"
+
+#ifdef _DEBUG
+#define new DEBUG_NEW
+#endif
+
+
+// CQQTangView
+
+IMPLEMENT_DYNCREATE(CQQTangView, CView)
+
+BEGIN_MESSAGE_MAP(CQQTangView, CView)
+	ON_WM_CONTEXTMENU()
+	ON_WM_RBUTTONUP()
+END_MESSAGE_MAP()
+
+// CQQTangView 构造/析构
+
+CQQTangView::CQQTangView()
+{
+	// TODO: 在此处添加构造代码
+
+}
+
+CQQTangView::~CQQTangView()
+{
+}
+
+BOOL CQQTangView::PreCreateWindow(CREATESTRUCT& cs)
+{
+	// TODO: 在此处通过修改
+	//  CREATESTRUCT cs 来修改窗口类或样式
+
+	return CView::PreCreateWindow(cs);
+}
+
+// CQQTangView 绘制
+
+void CQQTangView::OnDraw(CDC* /*pDC*/)
+{
+	CQQTangDoc* pDoc = GetDocument();
+	ASSERT_VALID(pDoc);
+	if (!pDoc)
+		return;
+
+	// TODO: 在此处为本机数据添加绘制代码
+}
+
+void CQQTangView::OnRButtonUp(UINT /* nFlags */, CPoint point)
+{
+	ClientToScreen(&point);
+	OnContextMenu(this, point);
+}
+
+void CQQTangView::OnContextMenu(CWnd* /* pWnd */, CPoint point)
+{
+#ifndef SHARED_HANDLERS
+	theApp.GetContextMenuManager()->ShowPopupMenu(IDR_POPUP_EDIT, point.x, point.y, this, TRUE);
+#endif
+}
+
+
+// CQQTangView 诊断
+
+#ifdef _DEBUG
+void CQQTangView::AssertValid() const
+{
+	CView::AssertValid();
+}
+
+void CQQTangView::Dump(CDumpContext& dc) const
+{
+	CView::Dump(dc);
+}
+
+CQQTangDoc* CQQTangView::GetDocument() const // 非调试版本是内联的
+{
+	ASSERT(m_pDocument->IsKindOf(RUNTIME_CLASS(CQQTangDoc)));
+	return (CQQTangDoc*)m_pDocument;
+}
+#endif //_DEBUG
+
+
+// CQQTangView 消息处理程序
---
+++ QQTang/QQTangView.h
@@ -0,0 +1,49 @@
+
+// QQTangView.h : CQQTangView 类的接口
+//
+
+#pragma once
+
+
+class CQQTangView : public CView
+{
+protected: // 仅从序列化创建
+	CQQTangView();
+	DECLARE_DYNCREATE(CQQTangView)
+
+// 特性
+public:
+	CQQTangDoc* GetDocument() const;
+
+// 操作
+public:
+
+// 重写
+public:
+	virtual void OnDraw(CDC* pDC);  // 重写以绘制该视图
+	virtual BOOL PreCreateWindow(CREATESTRUCT& cs);
+protected:
+
+// 实现
+public:
+	virtual ~CQQTangView();
+#ifdef _DEBUG
+	virtual void AssertValid() const;
+	virtual void Dump(CDumpContext& dc) const;
+#endif
+
+protected:
+
+// 生成的消息映射函数
+protected:
+	afx_msg void OnFilePrintPreview();
+	afx_msg void OnRButtonUp(UINT nFlags, CPoint point);
+	afx_msg void OnContextMenu(CWnd* pWnd, CPoint point);
+	DECLARE_MESSAGE_MAP()
+};
+
+#ifndef _DEBUG  // QQTangView.cpp 中的调试版本
+inline CQQTangDoc* CQQTangView::GetDocument() const
+   { return reinterpret_cast<CQQTangDoc*>(m_pDocument); }
+#endif
+
